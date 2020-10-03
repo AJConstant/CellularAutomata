3110ededfd02233753d22f5f36d3e77572210ff9
Alex Constant <alexanderjconstant@gmail.com>
2020-10-03 14:45:02 -0500

    Project Init. Ref #1

---
+++ cellular-automata/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div> Hey, World </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
